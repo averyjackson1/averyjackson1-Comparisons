f19a66c969d592055c60e734ad4e469059ce5cb0
averyjackson1 <jackson.avery11@gmail.com>
2023-01-27 19:32:30 +0000

    Done

--- src/main/java/Comparators.java
+++ src/main/java/Comparators.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertThrows;
 
 /**
  * If we want to make programs that "do something", we're going to need to create instructions that change
@@ -24,6 +25,9 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
+        if(a==b)
+        return true;
+        else
         return false;
     }
 
@@ -33,6 +37,9 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
+        if(a>b)
+        return true;
+        else
         return false;
     }
     /**
@@ -41,6 +48,9 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
+        if(a<b)
+        return true;
+        else
         return false;
     }
     /**
@@ -49,6 +59,9 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
+        if(a>=b)
+        return true;
+        else
         return false;
     }
     /**
@@ -57,6 +70,9 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
+        if(a<=b)
+        return true;
+        else
         return false;
     }
 
@@ -66,6 +82,9 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
+        if(a != b)
+        return true;
+        else
         return false;
     }
 }
